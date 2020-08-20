8ce43d35b2d175672414434d8fc64a2d74439dd3
Nicholas Foreman <nicholas.m.foreman@gmail.com>
2020-08-19 22:47:38 -0500

    Editing Atlas Connection

--- server.js
+++ server.js
@@ -18,11 +18,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+//  useNewUrlParser: true,
+//  useFindAndModify: false,
+//  useUnifiedTopology: true
+//});
+
+console.log("env", process.env.NODE_ENV);
+let uri = "mongodb://localhost/budget";
+if (process.env.NODE_ENV === "production") {
+  uri = process.env.MONGODB_URI;
+};
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // routes
 app.use(require("./routes/api.js"));
